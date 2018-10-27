313f57ea190e7f94eb16780a04e3189f0f9f6e91
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2018-10-27 20:58:26 +0300

    Update mini_clock_1_3.ino

--- mini_clock_1_3.ino
+++ mini_clock_1_3.ino
@@ -76,8 +76,8 @@ Button buttonB = Button(buton2, BUTTON_PULLUP);      // Setup button B (using bu
 // DHT sensor
 #define DHTPIN senzor     // what pin we're connected to
 // Uncomment whatever type you're using!
-#define DHTTYPE DHT11   // DHT 11 
-//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT11   // DHT 11 
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
